8b27a2933fdb05e86a443e3b9e96c8f60af733e8
albinhsn <albin.henningsson1@gmail.com>
2023-12-06 08:48:33 +0100

    day 6 in C

--- day6/6.c
+++ day6/6.c
@@ -1,11 +1,45 @@
 #include "../helpers.h"
+#include <math.h>
+#include <stdint.h>
 
 int solve_p1(char *content) {
-  struct Lines *lines = initLines();
-  parseLines(lines, "\n", content);
+  struct StrArray *lines = parseLines("\n", content);
+  struct LongArray *times = parseLongsFromLine(&lines->array[0]);
+  struct LongArray *distances = parseLongsFromLine(&lines->array[1]);
+  long answer = 1;
+  for (int i = 0; i < times->length; ++i) {
+    long score = 0;
+    long time = times->array[i];
+    long dist = distances->array[i];
+    for (int j = 1; j < time; j++) {
+      if (dist < j * (time - j)) {
+        score++;
+      }
+    }
+
+    answer *= score;
+  }
+  freeStringArray(lines);
+  freeArray(times);
+  freeArray(distances);
+  return answer;
 }
 
-long solve_p2(char *content) {}
+long solve_p2(char *content) {
+  struct StrArray *lines = parseLines("\n", content);
+  long answer = 0;
+  long time = concatDigitsFromLine(lines->array[0]);
+  long dist = concatDigitsFromLine(lines->array[1]);
+  for (int j = 1; j < time; j++) {
+    if (dist < j * (time - j)) {
+      answer++;
+    }
+  }
+
+  freeStringArray(lines);
+
+  return answer;
+}
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
