949ad9646502cc0388b60a8cc20d0728e41a9c57
albinhsn <albin.henningsson1@gmail.com>
2023-12-21 13:35:00 +0100

    wip p2 day 21

--- day21/21.py
+++ day21/21.py
@@ -1,7 +1,6 @@
 import sys
 import time
 from collections import defaultdict, deque
-from copy import deepcopy
 
 
 def solve_p1():
@@ -34,80 +33,87 @@ def solve_p1():
 
 
 def solve_p2():
+    def get_neighbours(x, y):
+        neighbours = set()
+        for xx, yy in XY:
+            grid = (0, 0)
+            XX = x + xx
+            YY = y + yy
+            if XX < 0:
+                XX += X
+                grid = (-1, 0)
+            elif XX >= X:
+                XX -= X
+                grid = (0, 1)
+            if YY < 0:
+                YY += Y
+                grid = grid[0], -1
+            elif YY >= Y:
+                YY -= Y
+                grid = grid[0], 1
+            if lines[YY][XX] == "#":
+                continue
+            neighbours.add((XX, YY, grid))
+        return neighbours
+
+    def print_map(plots):
+        for y in range(Y):
+            for x in range(X):
+                if lines[y][x] == "S":
+                    print("S", end="")
+                if (x, y) in plots:
+                    print("0", end="")
+                else:
+                    print(lines[y][x], end="")
+            print()
+
     time1 = time.time()
     lines = open(sys.argv[1]).read().strip().split("\n")
-    x, y = [
+    XX, YY = [
         [(idx, i) for idx, x in enumerate(y) if x == "S"][0]
         for i, y in enumerate(lines)
         if "S" in y
     ][0]
     XY = [(1, 0), (0, -1), (-1, 0), (0, 1)]
     X, Y = len(lines[0]), len(lines)
-    STEPS = 500
+    STEPS = 10
+
     plots1 = defaultdict(set)
     plots2 = defaultdict(set)
-    plots2[(x, y)].add((0, 0))
-    neighbours = defaultdict(set)
-    p2 = 1
-    for i in range(Y):
-        for j in range(X):
-            for xx, yy in XY:
-                tmp_x, tmp_y = j + xx, i + yy
-                gdx, gdy = (0, 0)
-                if tmp_y < 0:
-                    tmp_y = Y + tmp_y
-                    gdy = -1
-                elif tmp_y >= Y:
-                    tmp_y = tmp_y - Y
-                    gdy = 1
-                if tmp_x < 0:
-                    tmp_x = X + tmp_x
-                    gdx = -1
-                elif tmp_x >= X:
-                    tmp_x = tmp_x - X
-                    gdx = 1
-                if lines[tmp_x][tmp_y] == "#":
-                    continue
-                neighbours[(j, i)].add(((tmp_x, tmp_y), gdx, gdy))
-    visited = set()
-    previous = {}
-    for steps in range(STEPS // 2):
-        tmp_dict = defaultdict(set)
-        for pos, grids in plots2.items():
-            for xy, gdx, gdy in neighbours[pos]:
-                tmp_dict[xy].update(
-                    (i[0] + gdx, i[1] + gdy)
-                    for i in grids
-                    if (i[0] + gdx, i[1] + gdy) not in plots1[xy]
+    plots2[(XX, YY)].add((0, 0))
+    for _ in range(STEPS // 2):
+        for (x, y), val in plots2.items():
+            for XX, YY, grid in get_neighbours(x, y):
+                to_add = (
+                    set([g for g in val if g not in plots2[XX, YY]])
+                    if grid == (0, 0)
+                    else set(
+                        (grid[0] + g[0], grid[1] + g[1])
+                        for g in val
+                        if (grid[0] + g[0], grid[1] + g[1]) not in plots1[XX, YY]
+                    )
                 )
-        if tuple(tmp_dict.keys()) in visited:
-            # print(previous[tuple(tmp_dict.keys())]["steps"], steps)
-            prev = previous[tuple(tmp_dict.keys())]["dict"]
-            print([len(tmp_dict[key]) - len(prev[key]) for key in tmp_dict])
-            print("-"*15)
-        visited.add(tuple(tmp_dict.keys()))
-        previous[tuple(tmp_dict.keys())] = {"steps": steps, "dict": deepcopy(tmp_dict)}
-        plots1 = tmp_dict
-
-        tmp_dict = defaultdict(set)
-        for pos, grids in plots1.items():
-            for xy, gdx, gdy in neighbours[pos]:
-                tmp_dict[xy].update(
-                    (i[0] + gdx, i[1] + gdy)
-                    for i in grids
-                    if (i[0] + gdx, i[1] + gdy) not in plots2[xy]
+                if to_add:
+                    plots1[XX, YY].update(to_add)
+        print(f"plots1 {sum(len(i) for i in plots1.values())}", set(plots1.keys()))
+        print_map(plots1)
+        for (x, y), val in plots1.items():
+            for XX, YY, grid in get_neighbours(x, y):
+                to_add = (
+                    set(g for g in val if g not in plots2[XX, YY])
+                    if grid == (0, 0)
+                    else set(
+                        (grid[0] + g[0], grid[1] + g[1])
+                        for g in val
+                        if (grid[0] + g[0], grid[1] + g[1]) not in plots2[XX, YY]
+                    )
                 )
-        p2 += sum(len(i) for i in tmp_dict.values())
-        if tuple(tmp_dict.keys()) in visited:
-            # print(previous[tuple(tmp_dict.keys())], steps)
-            prev = previous[tuple(tmp_dict.keys())]["dict"]
-            print([len(tmp_dict[key]) - len(prev[key]) for key in tmp_dict])
-            print("-"*15)
-        visited.add(tuple(tmp_dict.keys()))
-        previous[tuple(tmp_dict.keys())] = {"steps": steps, "dict": deepcopy(tmp_dict)}
-        plots2 = tmp_dict
+                if to_add:
+                    plots2[XX, YY].update(to_add)
+        print(f"plots2 {sum(len(i) for i in plots2.values())}", set(plots2.keys()))
+        print_map(plots2)
 
-    print("p2", p2, f"took {time.time() - time1}")
+    print("p2", sum(len(i) for i in plots2.values()), f"took {time.time() - time1}")
 
 
 # solve_p1()
